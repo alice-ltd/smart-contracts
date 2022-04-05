5614b1bb59df2f182615a1b65fd3b07973ac8e6e
plusCubed <6379975+plusCubed@users.noreply.github.com>
2022-04-05 11:14:48 -0700

    change e2e test account to env var

--- utils/src/utils/testAccounts.ts
+++ utils/src/utils/testAccounts.ts
@@ -1,16 +1,12 @@
 import { MnemonicKey } from '@terra-money/terra.js';
 import { bombay } from './terra';
 
-// terra1u3lex2u3dnz6wvte0wjukqe0rphansptxatv32
 const mk1 = new MnemonicKey({
-  mnemonic:
-    'license hidden roast ensure make topple cloud mad offer crumble defy tuna sweet spray ten gas health about same chunk force about fabric rose',
+  mnemonic: process.env.TEST_MNEMONIC_1
 });
 export const wallet1 = bombay.wallet(mk1);
 
-// terra14t7mukq06ty7p64cr2xvhvkty56jfh7e70qnkg
 const mk2 = new MnemonicKey({
-  mnemonic:
-    'rain stick dress ride fatigue loud wrist address title demise speed fabric manage ensure basic vote enough voice century betray music shuffle witness door',
+  mnemonic: process.env.TEST_MNEMONIC_2,
 });
 export const wallet2 = bombay.wallet(mk2);
